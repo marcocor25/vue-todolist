953f265d832d1772add5f781ee90df38f5a63c52
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-25 21:37:04 +0100

    Ho creato lo stile, continuo domani

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-// TEST
-console.log('Ciao a tutti!');
+// COLLEGAMENTO VUE.JS
+const app = new Vue({
+    el: '#root',
+    data: {
+        title: 'title',
+        h1Content: 'to do list',
+        barWrapperClass: 'bar-wrapper',
+        inputType: 'text',
+        placeOlder: 'Inserisci qui le tue cose da fare...',
+        inputClass: 'input-text',
+        mainTitle: 'your tasks :',
+    }
+})
