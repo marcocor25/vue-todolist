78507e31971ca7407d554c529904c7c907f7c948
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-26 17:46:55 +0100

    Migliorato ulteriormente stile | Inizio il Vue

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = new Vue({
     el: '#root',
     data: {
         title: 'title',
-        h1Content: 'to do list',
+        sidebarTitle: 'to do list',
         barWrapperClass: 'bar-wrapper',
         inputType: 'text',
         placeOlder: 'Inserisci qui le tue cose da fare...',
         inputClass: 'input-text',
         mainTitle: 'your tasks :',
+        ulClass: 'todolist',
+        newTask: '',
     }
 })
